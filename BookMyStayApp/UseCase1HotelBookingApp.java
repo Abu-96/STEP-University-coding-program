3d5def364db9dbbb486161602b38ee4f0e19f5f9
Abu-96 <abubakr96.1221@gmail.com>
2026-03-24 08:29:24 +0530

    BookMyStayApp

---
+++ BookMyStayApp/UseCase1HotelBookingApp.java
@@ -0,0 +1,9 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Thank you for using our service. Enjoy your stay!");
+    }
+}
